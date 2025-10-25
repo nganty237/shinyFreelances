be8ab3de07807695d15d0fe0ba05faea60dce98e
nganty237 <surflinkayos@gmail.com>
2025-10-25 10:20:32 +0100

    commit test

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -25,7 +25,7 @@ function Error() {
               href="#"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
-              Go back home
+              back home
             </Link>
             <a href="#" className="text-sm font-semibold text-white">
               Contact support <span aria-hidden="true">&rarr;</span>
